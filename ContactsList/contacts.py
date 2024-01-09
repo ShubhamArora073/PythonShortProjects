7dbdd775a4ec4450968a249d340fc8d719b90266
Shubham Arora <shubham.arora073@gmail.com>
2024-01-09 13:37:41 +0530

    Code to add country to the contact list

--- ContactsList/contacts.py
+++ ContactsList/contacts.py
@@ -1,16 +1,16 @@
 import json
 
 
-def add_contact(user, age, address, phone):
+def add_contact(user, age, address, phone, country):
     new_contact = {
         "user": user,
         "age": age,
         "address": address,
         "phone": phone
     }
+    new_contact.update({"country": country})
     my_contacts.append(new_contact)
     return my_contacts
-
 
 # Initialize an empty contacts list
 my_contacts = []
@@ -19,17 +19,29 @@ my_contacts = []
 for i in range(2):
     # Get contact information from the user
     user = input("Enter the name of the contact: ")
-    age = input("Enter the age of the contact: ")
+    age = int(input("Enter the age of the contact: "))
     address = input("Enter the address of the contact: ")
-    phone = input("Enter the phone of the contact: ")
+    phone = int(input("Enter the phone of the contact: "))
+    country = input("Enter the country: ")
 
     # Add a new contact
-    my_contacts = add_contact(user, age, address, phone)
-
-# Convert the list of contacts to a JSON string
-contacts_json = json.dumps(my_contacts, indent=4)
+    my_contacts = add_contact(user, age, address, phone, country)
+#
+# for contact in my_contacts:
+#     if contact["age"] > 30:
+#         contacts_json = json.dumps(contact, indent=2)
+#         print("Here are your contacts:")
+#         print(contacts_json)
 
 # Write the JSON string to the file
 with open('contacts.html', 'w') as f:
     f.write('My Contacts are:\n')
-    f.write(contacts_json)
+    for contact in my_contacts:
+        if contact["age"] > 30:
+            contacts_json = json.dumps(contact, indent=2)
+            f.write(contacts_json)
+
+
+
+
+
